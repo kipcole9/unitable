750428acb0bb4461588c3de17ea944420d7eac23
Kip Cole <kipcole9@gmail.com>
2015-05-19 14:36:53 +0200

    Add test for min and max to ensure comparable is working

--- spec/unitable_spec.rb
+++ spec/unitable_spec.rb
@@ -8,4 +8,9 @@ describe Unitable do
   it 'knows 1 km in miles' do
     expect(Unit::Kilometer[1]).to eq(Unit::Mile[0.621371192237334])
   end
+  
+  it 'should work with max and min' do
+    expect([Unit::Metre[10], 50].max).to eq(50)
+    expect([100, Unit::Metre[50]].max).to eq(100)
+  end    
 end
